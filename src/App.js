6b9beffff68fdbc702d30023f1127a02b92e0df5
adamctr <adam.courtaro@gmail.com>
2022-10-04 18:07:13 +0200

    lifecycle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import { Component } from "react";
 import "./App.css";
-import Form from "./components/Form";
+import Car from "./components/Car";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Je suis dans le constructor");
+
+    this.state = {
+      name: "Toto",
+      step: 1,
+      display: true,
+    };
+  }
+  componentDidMount() {
+    console.log("Le composant a été monté");
+    this.setState({
+      step: 2,
+    });
+  }
+
+  componentWillUnmount() {
+    console.log("Le composant a été retiré");
+  }
+
+  displayOrHidden = () => {
+    this.setState({
+      display: !this.state.display,
+    });
+  };
+
   render() {
+    console.log("Je suis dans le render");
+    const showComponent = this.state.display ? (
+      <Car name="Toto 2"></Car>
+    ) : (
+      <div></div>
+    );
+
     return (
       <div className="App">
-        <Form></Form>
+        <button onClick={this.displayOrHidden}>
+          Enlever le composant du DOM
+        </button>
+        {showComponent}
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,65 +1,18 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 class Form extends Component {
-  state = {
-    username: "",
-    color: "",
-    colors: ["", "red", "blue", "green"],
-    comment: "",
-  };
-
-  handlePseudo = (e) => {
-    this.setState({
-      username: e.target.value,
-    });
-  };
-
-  handleColor = (e) => {
-    this.setState({
-      color: e.target.value,
-    });
-  };
-
-  handleComments = (e) => {
-    this.setState({
-      comment: e.target.value,
-    });
-  };
-
   render() {
     return (
       <div>
-        <h1>Commentaire</h1>
-        <form action="">
-          <div>
-            <label htmlFor="">Pseudo</label>
-            <input
-              type="text"
-              value={this.state.username}
-              onChange={this.handlePseudo}
-            />
-          </div>
-          <div>
-            <label htmlFor="">Couleur</label>
-            <select value={this.state.color} onChange={this.handleColor}>
-              {this.state.colors.map((color, index) => {
-                return (
-                  <option key={index} value={color}>
-                    {color}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <div>
-            <label htmlFor="">Commentaire</label>
-            <textarea
-              value={this.state.comment}
-              onChange={this.handleComments}
-            ></textarea>
-          </div>
-          <button>Valider</button>
-        </form>
+        <Title>Commentary</Title>
+        <button>Valider</button>
       </div>
     );
   }
